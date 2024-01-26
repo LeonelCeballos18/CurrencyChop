76317c93432e63bc208c8dd100b54a980ba2eaed
Leonel Alejandro Ceballos Alvarado <lceballos5@ucol.mx>
2024-01-26 17:25:55 -0600

    selectFinished

--- index.js
+++ index.js
@@ -6,16 +6,14 @@ import axios from "axios"
 const app = express();
 const port = 3000;
 const ApiUrl = "https://api.coingecko.com/api/v3"
-const apiKey = "?x_cg_demo_api_key=CG-F1TnaAfdS3FyGboUQ4kEsnTt";
-const config = {
-    headers : { "x-cg-pro-api-key" : "CG-F1TnaAfdS3FyGboUQ4kEsnTt" }
-};
+const apiKey = "x_cg_demo_api_key=CG-F1TnaAfdS3FyGboUQ4kEsnTt";
 
 app.use(express.static("public"));
 
 app.get("/", async function(req, res){
-    const result = await axios.get(ApiUrl + "/coins/list" + apiKey);
-    res.render("index.ejs", { content: JSON.stringify(result.data) });
+    const coins = await axios.get(ApiUrl + "/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=30&page=1&sparkline=false&locale=en&" + apiKey);
+    const exchangeRates = await axios.get(ApiUrl + "/exchange_rates?" + apiKey);
+    res.render("index.ejs", { coins: coins.data, rates: exchangeRates.data});
 })
 
 app.listen(port);
