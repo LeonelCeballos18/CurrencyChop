c789f5e280aca4e218ce1dd84317ed4cba61324f
Leonel Alejandro Ceballos Alvarado <lceballos5@ucol.mx>
2024-02-01 19:03:03 -0600

    Converter almost done

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import axios from "axios"
+import bodyParser from "body-parser";
 
 //API a usar https://currencyapi.com/docs
 
@@ -9,6 +10,7 @@ const ApiUrl = "https://api.coingecko.com/api/v3"
 const apiKey = "x_cg_demo_api_key=CG-F1TnaAfdS3FyGboUQ4kEsnTt";
 
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", async function(req, res){
     const coins = await axios.get(ApiUrl + "/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=30&page=1&sparkline=false&locale=en&" + apiKey);
@@ -17,7 +19,17 @@ app.get("/", async function(req, res){
 })
 
 app.post("/exchange", async function(req, res){
-    
+    try {
+        const coin = req.body.selectedCoin; //Its not the ID its the value of the coin, its needed to be changed
+        console.log(coin);
+        //const convertion = await axios.get(ApiUrl + `/coins/${coin}?tickers=false&market_data=true&` + apiKey); //Data to get convertion
+        //const coins = await axios.get(ApiUrl + "/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=30&page=1&sparkline=false&locale=en&" + apiKey); //All the coins
+        //const exchangeRates = await axios.get(ApiUrl + "/exchange_rates?" + apiKey); //All the exchange rates
+        res.render("index.ejs");
+        res.redirect("/")
+    } catch (error) {
+        res.status(404).send(error.message);
+    }
 })
 
 app.listen(port);
--- public/js/script.js
+++ public/js/script.js
@@ -1,20 +1,49 @@
 $(document).ready(function() {
-    $(".custom-select-trigger").click(function() {
-      $(".custom-options").toggle();
-    });
+  $(".custom-select-trigger").click(function(event) {
+    event.stopPropagation(); //prevents propagation of the same event from being called.
+    toggleOptions($(this));
+  });
+  
+  // Option selection
+  $(".custom-options li").click(function() {
+    var selectedOption = $(this).text();
+    updateTriggerText($(this).closest(".custom-select"), selectedOption);
+  });
+  
+  // Manage clicks outside of the area
+  $(document).on("click", function(event) {
+    if (!$(event.target).closest(".custom-select").length) {
+      hideOptions($(".custom-select, .custom-select-2"));
+    }
+  });
+  
+  function toggleOptions(trigger) {
+    let coinOptions = $("#custom-select-coin").next(".custom-options");
+    let exchangeOptions = $("#custom-select-exchange").next(".custom-options");
 
-    $(".custom-options li").click(function() {
-      var selectedOption = $(this).text();
-      $(".custom-select-trigger").text(selectedOption);
-      $(".custom-options").hide();
-    });
+    if (coinOptions.is(":visible") && trigger.attr("id") !== "custom-select-coin") {
+        coinOptions.toggle();
+    }
 
-    $(document).on("click", function(event) {
-      if (!$(event.target).closest(".custom-select").length) {
-        $(".custom-options").hide();
-      }
-    });
+    if (exchangeOptions.is(":visible") && trigger.attr("id") !== "custom-select-exchange") {
+        exchangeOptions.toggle();
+    }
 
+    let options = trigger.next(".custom-options");
+    options.toggle();
+  }
+  
+  function updateTriggerText(select, text) {
+    var trigger = select.find(".custom-select-trigger");
+    trigger.text(text);
+    trigger.val(text.val);
+    hideOptions(select);
+  }
+  
+  function hideOptions(select) {
+    var options = select.find(".custom-options");
+    options.hide();
+  }
     /*The input text is validated to receive only numbers and also so that the user can not copy, cut and paste
     something into the input
     */
@@ -28,9 +57,9 @@ $(document).ready(function() {
     })
     .on("input", function() {
       console.log(2);
-    });
+      const selectedCoin = $('#custom-select-exchange').val();
 
-    function updateConvertion(){
-      
-    }
+
+
+    });
   });
